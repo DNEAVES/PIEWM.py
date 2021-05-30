f7c4eae9ddfe4ee7ccc03586ffe5dc0f3082cde5
DNEAVES <DNEAVES94@gmail.com>
2021-05-30 13:36:38 -0400

    Update PIEWM.py

--- PIEWM.py
+++ PIEWM.py
@@ -12,20 +12,12 @@ pyfile = input("What is the file name? (Just the .py file): ")
 #Forgot the ".py"? We got you!
 if ".py" not in pyfile:
     pyfile = pyfile + ".py"
-#I like using the -F flag for making .exe's, keeps things compact
-onefile = input("Would you like to use the one-file flag? (Y/N): ")
-
+    
 #Move to directory
 sh.cd(pypath)
 
-#Show Progress bar while doing the work
+#Show progress bar while doing the work
 with alive_bar(1, title = "Working...", spinner = 'classic', length = 3) as bar:
-    if onefile.lower() == "y" or "yes" or "true":
-        sh.wine("cmd", "/c", "pyinstaller", "-F", pyfile)
-        bar()
-        print("Completed!")
-    else:
-        sh.wine("cmd", "/c", "pyinstaller", pyfile)
-        bar()
-        print("Completed!")
-    
+    sh.wine("cmd", "/c", "pyinstaller", "-F", pyfile)
+    bar()
+    print("Completed!")
